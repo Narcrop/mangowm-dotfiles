ba99606b4f5e690ccbf56c8f25b4820cbb384e3e
Narcrop <90524289+Narcrop@users.noreply.github.com>
2026-06-13 15:11:45 -0500

    Add files via upload

---
+++ .config/mango/modules/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set +e
+
+# noctalia
+noctalia
+
+# Keep clipboard content after app closes
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
+
+# Watch clipboard and store history
+wl-paste --type text --watch cliphist store &
